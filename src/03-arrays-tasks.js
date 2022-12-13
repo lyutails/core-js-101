201c221497e33be144bfec62bcf3c7b64f1771cb
lyutails <lyusya.cool@gmail.com>
2022-12-13 16:47:07 +0300

    feat: solve array of strings

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -86,8 +86,9 @@ function getArrayOfPositives(arr) {
  *    [ 1, 2, 3, 4, 5 ] => []
  *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
  */
-function getArrayOfStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfStrings(arr) {
+  const stringsArray = arr.filter((elem) => typeof elem === 'string');
+  return stringsArray;
 }
 
 /**
