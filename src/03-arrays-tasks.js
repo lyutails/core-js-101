19fd387a19aa5dca725e1fea62667a8359f927e5
lyutails <lyusya.cool@gmail.com>
2022-12-19 15:03:19 +0300

    feat: array select many

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -533,8 +533,10 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.reduce((accumArr, currVal) => {
+    return accumArr.concat(childrenSelector(currVal));
+  }, []);
 }
 
 
