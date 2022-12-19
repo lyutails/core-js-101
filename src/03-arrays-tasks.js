601ccd679c959916ba1321ede3991d6f0bc6bc43
lyutails <lyusya.cool@gmail.com>
2022-12-19 15:08:29 +0300

    feat: another desicion for select many in arrays

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -534,9 +534,10 @@ function group(/* array, keySelector, valueSelector */) {
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-  return arr.reduce((accumArr, currVal) => {
-    return accumArr.concat(childrenSelector(currVal));
-  }, []);
+  // return arr.reduce((accumArr, currVal) => {
+  //   return accumArr.concat(childrenSelector(currVal));
+  // }, []);
+  return arr.flatMap(childrenSelector);
 }
 
 
