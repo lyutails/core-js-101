01c6974bf4d97379e0406a4723f4c814932659da
lyutails <lyusya.cool@gmail.com>
2022-12-23 12:29:30 +0300

    feat: array moving sum

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -240,8 +240,13 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  const result = [arr[0]];
+  arr.reduce((accumulator, current) => {
+    result.push(accumulator + current);
+    return accumulator + current;
+  });
+  return result;
 }
 
 /**
