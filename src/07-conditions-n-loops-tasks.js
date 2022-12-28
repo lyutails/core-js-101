ccce1a1fef5a62a380c7fadf4f20b903d524ed3c
lyutails <lyusya.cool@gmail.com>
2022-12-29 02:19:18 +0300

    feat: point in circle

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -163,8 +163,12 @@ function doRectanglesOverlap(rect1, rect2) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const difX = Math.abs(circle.center.x - point.x);
+  const difY = Math.abs(circle.center.y - point.y);
+  const { radius } = circle;
+  if (Math.hypot(difX, difY) < radius) return true;
+  return false;
 }
 
 
