c771f02ab5d5699f8d560355de396684d6fc5e99
lyutails <lyusya.cool@gmail.com>
2022-12-29 00:03:16 +0300

    feat: rectangle overlap

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -129,8 +129,11 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  // eslint-disable-next-line no-mixed-operators
+  if (rect1.top > rect2.width || rect2.top > rect1.width) return false;
+  if (rect1.left > rect2.height || rect2.left > rect1.height) return false;
+  return true;
 }
 
 
