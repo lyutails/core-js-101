15191a984052f848b9e69df4a73e41c114efa3d0
lyutails <lyusya.cool@gmail.com>
2022-12-29 14:59:39 +0300

    feat: digital root

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -292,8 +292,9 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  // eslint-disable-next-line no-mixed-operators
+  return (num - 1) % 9 + 1;
 }
 
 
